45c5d7b86df3a0019b70ef404aaac0f57e7c1c44
Tanner Jones <tjones25@basecampcodingacademy.org>
2025-01-13 10:44:10 -0600

    loopnbranch

--- src/CoffeeShop.java
+++ src/CoffeeShop.java
@@ -16,12 +16,29 @@ public class CoffeeShop {
         // Get initial price of a cup of coffee
         System.out.print("\nWhat do you want to charge per cup of coffee? ");
         double price = scanner.nextDouble();
+        // Get hours via loop and branch
+        String givehours = scanner.nextLine();
+        while (!givehours.equalsIgnoreCase("y") && !givehours.equalsIgnoreCase("n")) {
+            System.out.print("Will your coffee shop be open 24 hours? (y/n)");
+            givehours = scanner.nextLine();
+        }
+        boolean hours = false; // Declare the boolean variable outside the if statement
+        if (givehours.equalsIgnoreCase("y")) {
+            hours = true;
+        } else if (givehours.equalsIgnoreCase("n")) {
+            hours = false;
+        }
 
         // Display what we have
         System.out.println("\nGreat. Here's what we've collected so far.");
         // Display name and shop name
         System.out.println("\nYour name is " + name + " and you're opening " + shopname + "!");
         // Display the price of one cup of coffee formatted in a monetary format
+        if (hours) {
+            System.out.println("\nYour coffee shop will be open 24/7!");
+        } else {
+            System.out.println("\nYour coffee shop will only be open during the day.");
+        }
         System.out.printf("Your first cup of coffee will sell for $%.2f.\n", price);
 
         scanner.close();
